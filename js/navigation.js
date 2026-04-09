71edcaed3c08402cb261c2bb47ca23ac3ed465fb
Asylhum74 <asylhum74@gmail.com>
2026-04-09 09:32:13 +0200

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,19 +1,33 @@
-const Shopping = {
-  list: [],
+// ------------------- GESTION BOUTON ----------------
+const buttons = document.querySelectorAll(".nav-btn");
 
-  init() {
-    this.generate();
-  },
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    buttons.forEach(btn => btn.classList.remove("active"));
+    button.classList.add("active");
 
-  generate() {
-    let list = [];
+  });
+});
+	
+// ------------------- MODE SOMBRE -------------------
+if (localStorage.getItem("darkMode") === "true") {
+  document.body.classList.add("dark");
+}
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+  localStorage.setItem("darkMode", document.body.classList.contains("dark"));
+}
 
-    Inventory.items.forEach(item => {
-      if (item.quantity <= 0) {
-        list.push(item.name);
-      }
-    });
-
-    this.list = list;
+/* =========================
+   Navigation
+========================= */
+function afficherPage(page) {
+  document.querySelectorAll(".card").forEach(p => p.style.display = "none");
+  document.getElementById("page-" + page).style.display = "block";
+  if(page === "proprete") {
+    afficherProprete();
   }
-};
+  if(page === "historique") {
+    afficherHistorique();
+  }
+}
